eb68bdff211d09efb6111193c3725f46f67b793c
saymont <saymonp.trevisan@gmail.com>
2019-10-03 23:30:33 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require('dotenv/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const routes = require('./routes');
+
+const app = express();
+// process.env.DATABASE
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// req.query = Acessar query params(para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
