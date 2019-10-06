9933d1d5ae4722ef5dfbcf9b3950c20b54f43e2d
saymonp <saymonp.trevisan@gmail.com>
2019-10-06 20:25:07 -0300

    Funcionalidades avançadas

--- src/server.js
+++ src/server.js
@@ -4,12 +4,32 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const routes = require('./routes');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 // process.env.DATABASE
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 })
 
 // req.query = Acessar query params(para filtros)
@@ -21,4 +41,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
